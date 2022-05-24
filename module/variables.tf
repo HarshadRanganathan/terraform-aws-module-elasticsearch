905af316c85505e425c167da882db0b39685599b
Harshad Ranganathan <32612787+HarshadRanganathan@users.noreply.github.com>
2022-05-24 01:51:15 +0100

    Create variables.tf

---
+++ module/variables.tf
@@ -0,0 +1,313 @@
+variable "namespace" {
+  type        = string
+  description = "Namespace (e.g. `eg` or `cp`)"
+  default     = ""
+}
+
+variable "stage" {
+  type        = string
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+  default     = ""
+}
+
+variable "name" {
+  type        = string
+  description = "Name of the application"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
+}
+
+variable "enabled" {
+  type        = bool
+  default     = true
+  description = "Set to false to prevent the module from creating any resources"
+}
+
+variable "security_groups" {
+  type        = list(string)
+  default     = []
+  description = "List of security group IDs to be allowed to connect to the cluster"
+}
+
+variable "allowed_cidr_blocks" {
+  type        = list(string)
+  default     = []
+  description = "List of CIDR blocks to be allowed to connect to the cluster"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "Subnet IDs"
+}
+
+variable "dns_zone_id" {
+  type        = string
+  default     = ""
+  description = "Route53 DNS Zone ID to add hostname records for Elasticsearch domain and Kibana"
+}
+
+variable "elasticsearch_version" {
+  type        = string
+  default     = "6.5"
+  description = "Version of Elasticsearch to deploy"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t2.small.elasticsearch"
+  description = "Elasticsearch instance type for data nodes in the cluster"
+}
+
+variable "instance_count" {
+  type        = number
+  description = "Number of data nodes in the cluster"
+  default     = 4
+}
+
+variable "iam_role_arns" {
+  type        = list(string)
+  default     = []
+  description = "List of IAM role ARNs to permit access to the Elasticsearch domain"
+}
+
+variable "iam_authorizing_role_arns" {
+  type        = list(string)
+  default     = []
+  description = "List of IAM role ARNs to permit to assume the Elasticsearch user role"
+}
+
+variable "iam_actions" {
+  type        = list(string)
+  default     = []
+  description = "List of actions to allow for the IAM roles, _e.g._ `es:ESHttpGet`, `es:ESHttpPut`, `es:ESHttpPost`"
+}
+
+variable "zone_awareness_enabled" {
+  type        = bool
+  default     = true
+  description = "Enable zone awareness for Elasticsearch cluster"
+}
+
+variable "availability_zone_count" {
+  type        = number
+  default     = 2
+  description = "Number of Availability Zones for the domain to use."
+}
+
+variable "ebs_enabled" {
+  type        = bool
+  description = "enable ebs volume type for es nodes"
+  default     = true
+}
+
+variable "ebs_volume_size" {
+  type        = number
+  description = "EBS volumes for data storage in GB"
+  default     = 0
+}
+
+variable "ebs_volume_type" {
+  type        = string
+  default     = "gp2"
+  description = "Storage type of EBS volumes"
+}
+
+variable "ebs_iops" {
+  type        = number
+  default     = 0
+  description = "The baseline input/output (I/O) performance of EBS volumes attached to data nodes. Applicable only for the Provisioned IOPS EBS volume type"
+}
+
+variable "encrypt_at_rest_enabled" {
+  type        = bool
+  default     = true
+  description = "Whether to enable encryption at rest"
+}
+
+variable "encrypt_at_rest_kms_key_id" {
+  type        = string
+  default     = ""
+  description = "The KMS key ID to encrypt the Elasticsearch domain with. If not specified, then it defaults to using the AWS/Elasticsearch service KMS key"
+}
+
+variable "log_publishing_index_enabled" {
+  type        = bool
+  default     = false
+  description = "Specifies whether log publishing option for INDEX_SLOW_LOGS is enabled or not"
+}
+
+variable "log_publishing_search_enabled" {
+  type        = bool
+  default     = false
+  description = "Specifies whether log publishing option for SEARCH_SLOW_LOGS is enabled or not"
+}
+
+variable "log_publishing_application_enabled" {
+  type        = bool
+  default     = false
+  description = "Specifies whether log publishing option for ES_APPLICATION_LOGS is enabled or not"
+}
+
+variable "log_publishing_index_cloudwatch_log_group_arn" {
+  type        = string
+  default     = ""
+  description = "ARN of the CloudWatch log group to which log for INDEX_SLOW_LOGS needs to be published"
+}
+
+variable "log_publishing_search_cloudwatch_log_group_arn" {
+  type        = string
+  default     = ""
+  description = "ARN of the CloudWatch log group to which log for SEARCH_SLOW_LOGS needs to be published"
+}
+
+variable "log_publishing_application_cloudwatch_log_group_arn" {
+  type        = string
+  default     = ""
+  description = "ARN of the CloudWatch log group to which log for ES_APPLICATION_LOGS needs to be published"
+}
+
+variable "automated_snapshot_start_hour" {
+  type        = number
+  description = "Hour at which automated snapshots are taken, in UTC"
+  default     = 0
+}
+
+variable "dedicated_master_enabled" {
+  type        = bool
+  default     = false
+  description = "Indicates whether dedicated master nodes are enabled for the cluster"
+}
+
+variable "dedicated_master_count" {
+  type        = number
+  description = "Number of dedicated master nodes in the cluster"
+  default     = 0
+}
+
+variable "dedicated_master_type" {
+  type        = string
+  default     = "t2.small.elasticsearch"
+  description = "Instance type of the dedicated master nodes in the cluster"
+}
+
+variable "advanced_options" {
+  type        = map(string)
+  default     = {}
+  description = "Key-value string pairs to specify advanced configuration options"
+}
+
+variable "es_subdomain_name" {
+  type        = string
+  default     = "elasticsearch"
+  description = "The name of the subdomain for ES in the DNS zone (_e.g._ `es`, `pes.elasticsearch`)"
+}
+
+variable "kibana_subdomain_name" {
+  type        = string
+  default     = "kibana"
+  description = "The name of the subdomain for Kibana in the DNS zone (_e.g._ `kibana`, `ui`, `ui-es`, `search-ui`, `kibana.elasticsearch`)"
+}
+
+variable "node_to_node_encryption_enabled" {
+  type        = bool
+  default     = false
+  description = "Whether to enable node-to-node encryption"
+}
+
+variable "cognito_enabled" {
+  type        = bool
+  default     = false
+  description =  "whether to enable cognito authentication"
+}
+
+variable "cognito_identity_pool_id" {
+  type        = string
+  description = "cognito identity pool id, the IDP to use"
+}
+
+variable "cognito_role_arn" {
+  type        = string
+  description = "AWS role ARN, role to access cognito from Elasticsearch"
+}
+
+variable "cognito_user_pool_id" {
+  type        = string
+  description = "the cognito user pool that will have access to login to kibana"
+}
+
+variable "enforce_https" {
+  type        = bool
+  description = "Whether or not to require HTTPS"
+}
+
+variable "tls_security_policy" {
+  type        = string
+  description = "The name of the TLS secuirty policy that needs to be applied to the HTTPS endpoint. Valid values: Policy-Min-TLS-1-0-2019-07 and Policy-Min-TLS-1-2-2019-07"
+}
+
+variable "custom_endpoint_certificate_arn" {
+  type        = string
+  description = "The name of the Custom endpoint certificate arn"
+}
+
+variable "custom_endpoint_enabled" {
+  type        = bool
+  default     = false
+  description = "Boolean value of custom endpoint and default is false"
+}
+
+variable "custom_endpoint" {
+  type        = string
+  description = "The name of the Custom endpoint"
+}
+
+variable "enable_advanced_security_options" {
+  type        = bool
+  description = "(Forces new resource) Whether advanced security is enabled"
+  default     = false
+}
+
+variable "enable_internal_user_database" {
+  type        = bool
+  description = "Whether the internal user database is enabled"
+  default     = false
+}
+
+variable "master_user_arn" {
+  type        = string
+  description = "ARN for the master user. Only specify if internal_user_database_enabled is not set or set to false"
+  default     = ""
+}
+
+variable "master_user_name" {
+  type        = string
+  description = "The master user's username, which is stored in the Amazon Elasticsearch Service domain's internal database. Only specify if internal_user_database_enabled is set to true"
+  default     = ""
+}
+
+variable "master_user_password" {
+  type        = string
+  description = "The master user's password, which is stored in the Amazon Elasticsearch Service domain's internal database. Only specify if internal_user_database_enabled is set to true"
+  default     = ""
+}
